70f4c56cd2124f099df309801b13a2af1c1eda2f
sri123cvbcvb <sriramjeeva01@gmail.com>
2026-09-09 14:27:25 +0530

    chore(init): initialize Spring Boot project structure with Maven configuration

---
+++ src/test/java/com/hrms/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrms.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
